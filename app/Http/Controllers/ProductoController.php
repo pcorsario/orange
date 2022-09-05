6ffdc83cd79412b0b80678f065aaadeeed2c4247
Patricio Vaca Escobar <patriciovacaescobar@gmail.com>
2022-09-05 11:46:57 -0500

    Added Productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -14,7 +14,7 @@ class ProductoController extends Controller
      */
     public function index()
     {
-        $productos=Producto::all();
+        $productos=Producto::paginate(1);
         //dd($productos);
         return view('productos.index', compact('productos'));
     }
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,4 +1,42 @@
-@foreach($productos as $producto)
-{{$producto->nombre}} {{$producto->precio}} {{$producto->categoria}}
-<br>
-@endforeach
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Productos</div>
+
+                <div class="card-body">
+
+                     
+                    <br>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Precio</th>
+                                <th scope="col">Categoria</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($productos as $producto)
+                            <tr>
+                                <th scope="row"> {{$producto->id}}</th>
+                                <td>{{$producto->nombre}}</td>
+                                <td>{{$producto->precio}}</td>
+                                <td> {{$producto->categoria}}</td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                    {{$productos->links()}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
